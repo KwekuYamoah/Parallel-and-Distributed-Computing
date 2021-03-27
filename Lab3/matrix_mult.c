42db0c342d279bbae6c1e29f2c13b18832fb267e
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-03-27 21:26:18 +0000

    matrix allocation,display, ini added

--- Lab3/matrix_mult.c
+++ Lab3/matrix_mult.c
@@ -35,3 +35,62 @@ int **matrixMemoryAllocate(int);
 
 int matrix_size;//global variable used by diagonal pthreads
 int matrix_size= N; //work around to make matrix size globally accesible
+
+/**
+ * @brief dynamicall creates a 2 dimensional array
+ * of any size
+ * 
+ * @param size 
+ * @return int** 
+ */
+int **matrixMemoryAllocate(int size){
+    //allocate space
+    int **matrix;
+    matrix = malloc(size * sizeof(int*));
+
+    for (int i = 0; i < size; i++){
+        matrix[i] = malloc(size * sizeof(int));
+    }
+    
+    /*if allocation failed*/
+    if(matrix == NULL){
+        fprintf(stderr, "out of memory\n");
+        exit(1);
+    }
+
+    /*if successful*/
+    return matrix;
+
+}
+
+/**
+ * @brief initialise matrix 
+ * 
+ * @param matrix 
+ * @param size 
+ */
+void matrixInitialise(int **matrix, int size){
+    int k = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j ++){
+           k ++;
+           matrix[i][j] = k;
+        }
+    }
+}
+
+/**
+ * @brief display matrix contents
+ * 
+ * @param matrix 
+ * @param size 
+ */
+void matrixDisplay(int **matrix, int size){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j ++){
+           printf("%d \t", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
